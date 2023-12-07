28afe1d818a1442a832b35c0d14cf150e2550646
dieg00s0ri0 <diego9.5.2000jios@gmail.com>
2023-12-07 14:22:40 -0600

    1422071223

--- js/info_RG022014.js
+++ js/info_RG022014.js
@@ -1,36 +1,42 @@
 /* global AFRAME */
-
 AFRAME.registerComponent('panel-rg022014', {
   init: function () {
     var buttonEls = document.querySelectorAll('#button-RG022014');
 
-    // Instanciamos las entidades a las variables
-    this.backgroundEl = document.querySelector('#close');
-    this.panel008031El = document.querySelector('[panel-rg008031]');
+    //instanciamos las entidades a las variables
+    this.movieImageEl;
+    //this.movieTitleEl = document.querySelector('#movieTitle');
+    //this.movieDescriptionEl = document.querySelector('#movieDescription');
+
+    
 
     this.onMenuButtonClick = this.onMenuButtonClick.bind(this);
     this.onBackgroundClick = this.onBackgroundClick.bind(this);
-
+    // seleccionamos el boton de cerrar
+    this.backgroundEl = document.querySelector('#close');
     for (var i = 0; i < buttonEls.length; ++i) {
       buttonEls[i].addEventListener('click', this.onMenuButtonClick);
     }
     this.backgroundEl.addEventListener('click', this.onBackgroundClick);
     this.el.object3D.renderOrder = 9999999;
     this.el.object3D.depthTest = false;
+   
   },
 
   onMenuButtonClick: function (evt) {
-    // Ocultar el panel-rg008031 si está abierto
-    if (this.panel008031El) {
-      this.panel008031El.setAttribute('visible', false);
-    }
+  //   AFRAME.registerComponent('panel-rg022014', {
+  //     init: function () {
+  // this.el.object3D.visible = false;
+  //     }
+  // });
 
-    // Mostrar el panel actual
     this.backgroundEl.object3D.scale.set(1, 1, 1);
-    if (AFRAME.utils.device.isMobile()) { 
-      this.el.object3D.scale.set(1.4, 1.4, 1.4);
-    }
+
+    // this.el.object3D.scale.set(1, 1, 1);
+    if (AFRAME.utils.device.isMobile()) { this.el.object3D.scale.set(1.4, 1.4, 1.4); }
     this.el.object3D.visible = true;
+
+    
   },
 
   onBackgroundClick: function (evt) {
